0505740879d5f1235690206978c023ba9178e8db
joshua-unseen <corpselight@gmail.com>
2019-07-17 21:27:41 -0700

    Building the controller.  Find by ID functions outlined.

--- controllers/dbcontroller.js
+++ controllers/dbcontroller.js
@@ -3,20 +3,29 @@ const db = require("../models");
 
 // export methods
 module.exports = {
-    // Users
+// Users
     findUsers: function(req, res) {
         // pass an empty object ({}) in req.query to find all users.
         // pass {username: the_username} or {email: the_email} to search by username or email
         db.User.find(req.query).then().catch();
     },
-    // Beers
+    findUserByID: function(req, res) {
+        db.User.findById(req.params.id).populate().then().catch();
+    },
+// Beers
     findBeer: function(req, res) {
         // pass an empty object ({}) in req.query to find all beers.
         db.Beer.find(req.query).then().catch();
     },
-    // Comments
+    findBeerByID: function(req, res) {
+        db.Beer.findById(req.params.id).populate().then().catch();
+    },
+// Comments
     findComments: function(req, res) {
         // pass an empty object ({}) in req.query to find all comments.
         db.Comment.find(req.query).then().catch();
+    },
+    findCommentByID: function(req, res) {
+        db.User.findById(req.params.id).populate().then().catch();
     }
 };
