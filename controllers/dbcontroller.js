9bdb5008e7dfbfeefcada72509460a6f5329b89d
joshua-unseen <corpselight@gmail.com>
2019-07-17 21:17:05 -0700

    starting on the controller

--- controllers/dbcontroller.js
+++ controllers/dbcontroller.js
@@ -1,2 +1,22 @@
 // hook the ORM
 const db = require("../models");
+
+// export methods
+module.exports = {
+    // Users
+    findUsers: function(req, res) {
+        // pass an empty object ({}) in req.query to find all users.
+        // pass {username: the_username} or {email: the_email} to search by username or email
+        db.User.find(req.query).then().catch();
+    },
+    // Beers
+    findBeer: function(req, res) {
+        // pass an empty object ({}) in req.query to find all beers.
+        db.Beer.find(req.query).then().catch();
+    },
+    // Comments
+    findComments: function(req, res) {
+        // pass an empty object ({}) in req.query to find all comments.
+        db.Comment.find(req.query).then().catch();
+    }
+};
