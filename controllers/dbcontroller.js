0d9b0d84f075f84c37f9e5d6942723afe28cd6db
joshua-unseen <corpselight@gmail.com>
2019-07-20 12:32:20 -0700

    Basic controller methods complete

--- controllers/dbcontroller.js
+++ controllers/dbcontroller.js
@@ -3,29 +3,43 @@ const db = require("../models");
 
 // export methods
 module.exports = {
-// Users
-    findUsers: function(req, res) {
+    // Users
+    findUsers: function (req, res) {
         // pass an empty object ({}) in req.query to find all users.
         // pass {username: the_username} or {email: the_email} to search by username or email
-        db.User.find(req.query).then().catch();
+        db.User.find(req.query).
+            then((dbRes) => { res.json(dbRes); }).
+            catch((err) => { res.status(422).json(err); });
     },
-    findUserByID: function(req, res) {
-        db.User.findById(req.params.id).populate().then().catch();
+    findUserByID: function (req, res) {
+        db.User.findById(req.params.id).populate().
+            then((dbRes) => { res.json(dbRes); }).
+            catch((err) => { res.status(422).json(err); });
     },
-// Beers
-    findBeer: function(req, res) {
+    // Beers
+    findBeer: function (req, res) {
         // pass an empty object ({}) in req.query to find all beers.
-        db.Beer.find(req.query).then().catch();
+        db.Beer.find(req.query).
+            then((dbRes) => { res.json(dbRes); }).
+            catch((err) => { res.status(422).json(err); });
     },
-    findBeerByID: function(req, res) {
-        db.Beer.findById(req.params.id).populate().then().catch();
+    findBeerByID: function (req, res) {
+        db.Beer.findById(req.params.id).
+            populate().
+            then((dbRes) => { res.json(dbRes); }).
+            catch((err) => { res.status(422).json(err); });
     },
-// Comments
-    findComments: function(req, res) {
+    // Comments
+    findComments: function (req, res) {
         // pass an empty object ({}) in req.query to find all comments.
-        db.Comment.find(req.query).then().catch();
+        db.Comment.find(req.query).
+            then((dbRes) => { res.json(dbRes); }).
+            catch((err) => { res.status(422).json(err); });
     },
-    findCommentByID: function(req, res) {
-        db.User.findById(req.params.id).populate().then().catch();
+    findCommentByID: function (req, res) {
+        db.User.findById(req.params.id).
+            populate().
+            then((dbRes) => { res.json(dbRes); }).
+            catch((err) => { res.status(422).json(err); });
     }
 };
