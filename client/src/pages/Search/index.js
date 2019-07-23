f7a0be659f58f217b61bd569a5a7538db730260b
Sarah B <smblair87@gmail.com>
2019-07-22 20:59:02 -0700

    Added dropdown to Beer ABV field and moved style sheets from components to page

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -3,6 +3,7 @@ import Jumbotron from '../../components/Jumbotron';
 import { Container, Row, Col } from '../../components/Grid';
 import Style from '../../components/BeerStyle';
 import ABV from '../../components/BeerABV';
+import './style.css';
 
 class SearchBeers extends Component {
   // Creates state
@@ -19,6 +20,7 @@ class SearchBeers extends Component {
         <Jumbotron>
           <h1>Hop to It</h1>
         </Jumbotron>
+        <h1>Beer Style:</h1>
         <Style
           suggestions={[
             "IPA",
@@ -30,7 +32,33 @@ class SearchBeers extends Component {
             "Amber"
           ]}
         />
-        <ABV></ABV>
+        <h1>Beer ABV % (Optional):</h1>
+        <ABV
+          suggestions={[
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9", 
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22"
+          ]}
+        />
       </Container>
     );
   }
