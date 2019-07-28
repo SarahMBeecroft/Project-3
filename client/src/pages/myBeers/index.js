e33abec1494fdde6634de2473ba64fef8420f7bf
Sarah B <smblair87@gmail.com>
2019-07-28 16:05:45 -0700

    Started working on function to save beers to My Beers page, results are saving, but not yet displaying

--- client/src/pages/myBeers/index.js
+++ client/src/pages/myBeers/index.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import Jumbotron from '../../components/Jumbotron';
 import { Container, Row, Col } from '../../components/Grid';
+import API from '../../utils/API';
+import SavedBeer from '../../components/MyBeers';
 
 class MyBeers extends Component {
   // Creates state
   state = {
-    beers: [],
-    error: '',
-    message: ''
+    savedBeers: []
   };
+
+  componentDidMount() {
+    API.getBeers()
+
+      .then(res => this.setState({ savedBeers: res.data }))
+      .catch(err => console.log(err))
+  }
+
+  handleDeleteButton = id => {
+    API.deleteBeers(id)
+      .then(res => this.componentDidMount())
+      .catch(err => console.log(err))
+  }
 
   render() {
     return (
@@ -16,7 +29,10 @@ class MyBeers extends Component {
         <Jumbotron>
           <h1>Hop to It</h1>
         </Jumbotron>
-    
+        <SavedBeer
+            savedBeers={this.state.savedBeers}
+            handleDeleteButton={this.handleDeleteButton}
+          />
       </Container>
     );
   }
