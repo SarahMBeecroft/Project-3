cca15b318cf93c8075f7c57c43c96296b921be62
joshua-unseen <corpselight@gmail.com>
2019-07-28 16:56:11 -0700

    added label display

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -20,7 +20,10 @@ const SearchResults = props => {
                 <li className='search-list list-group-item' key={beer._id}>
                   <Row className='SearchResult row' id={beer.name + 'Card'}>
                     <Col size='2' className='beerImage'>
-                     <img src={'https://cdn.pastemagazine.com/www/articles/2019/07/01/hazy-IPA-generic-main.jpg'} alt='pint of beer'/> 
+                     <img src={
+                       beer.label ? beer.label :
+                       'https://cdn.pastemagazine.com/www/articles/2019/07/01/hazy-IPA-generic-main.jpg'
+                       } alt='pint of beer'/> 
                     </Col>
                     <Col size='1' className='emptyCol' />
                     <Col size='9' className='beerInfo'>
