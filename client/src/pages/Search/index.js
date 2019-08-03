a6513831230151a0631afba365c142495660463a
benitezmarcos <marcosbenitez@D-10-19-13-72.dhcp4.washington.edu>
2019-08-03 11:55:01 -0700

    layout and design edits

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -7,6 +7,7 @@ import './style.css';
 import SearchResults from '../../components/Results';
 import { set } from 'mongoose';
 import { AppContext } from "../../components/AppContainer";
+import icon4 from "../../components/AvatarImg/img/icon4.png";
 
 /*******************
  * 
@@ -119,16 +120,34 @@ class SearchBeers extends Component {
     }
 
     return (
-      <section id="search" className="section section-search darken-1 white-text scrollspy">
+      <section id="search" className="section section-search darken-1 scrollspy">
         <Container fluid>
-          <Jumbotron>
-            <h1 className="title">Hop to It</h1>
-          </Jumbotron>
-          <h4>What kind of beer are you looking for?</h4>
-          <Style
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-          />
+          <div className="row">
+            <div className="col s12">
+              <Jumbotron>
+                <h1 className="title">Hop to It</h1>
+
+                <img className="circle bigIcon center-align" src={icon4}></img>
+
+              </Jumbotron>
+            </div>
+          </div>
+
+          <div className="row jumbotron2">
+            <div className="col s12">
+              <Jumbotron>
+                <h4>What kind of beer are you looking for?</h4>
+                <Style
+                  handleFormSubmit={this.handleFormSubmit}
+                  handleInputChange={this.handleInputChange}
+                />
+              </Jumbotron>
+            </div>
+          </div>
+
+
+
+
           {/* // suggestions={[
           //   "Ale",
           //   "India Pale Ale",
@@ -177,7 +196,8 @@ class SearchBeers extends Component {
           handleFormSubmit={this.handleFormSubmit}
           handleInputChange={this.handleInputChange}
       /> */}
-          <h5>Your personalized beer results:</h5>
+      
+          {/* <h5>Your personalized beer results:</h5> */}
           <SearchResults
             beers={this.state.beers}
             // Save button isn't functional yet
