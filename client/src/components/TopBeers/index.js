2a4c38d1b4a3362ab30628336119202db14ae1da
Sarah B <smblair87@gmail.com>
2019-08-04 13:59:08 -0700

    Added grid layout for top beers page

--- client/src/components/TopBeers/index.js
+++ client/src/components/TopBeers/index.js
@@ -9,16 +9,17 @@ const TopBeer = props => {
     <div className='card'>
       <div className='card-body player'>
         <div className='result'>
-          <h3>Check out favorite beers:</h3>
         </div>
       </div>
     </div>
   ) : (
-      <div className='card-body player'>
-        <div className='result'>
-          <h3>Top Beers:</h3>
+    <div className='cards'>
+      <div className='card-body'>
+        <div className='result grid-container'>
           {props.topBeers.map(topBeer => {
             return (
+                <div className='border'>
+                <div className='cardContent'>
                 <li className='search-list list-group-item' key={topBeer._id}>
                   <Row className='SearchResult row' id={topBeer._id + 'Card'}>
                     <Col size='2' className='beerImage'>
@@ -31,30 +32,41 @@ const TopBeer = props => {
                     </Col>
                     <Col size='1' className='emptyCol' />
                     <Col size='9' className='beerInfo'>
+                      <Row className='beerInfo'>
 
-                      <Row>
                         <h5 className='beerName'>{topBeer.name}</h5>
-                      </Row>
 
-                      <Row>
                         <h6 className='abv'>ABV: {topBeer.abv}</h6>
-                      </Row>
-                      <Row>
-                        <p className='description'>{topBeer.description}</p>
+
+                        <p className='description'>{topBeer.description}</p>     
+
+                        <p className='breweryName'><b>Brewery:</b> {topBeer.brewery.name}</p>
+
+                        <a href={topBeer.brewery.website} target='_blank'><p className='breweryweb'>Brewery Website</p></a>  
+
+                        <p className='brewerylocation'><b>Brewery Address:</b> {topBeer.brewery.location}</p>
+
                       </Row>
                     </Col>
                   </Row>
-                <br></br>
+             
                 <Row className='buttonDiv'>
+                <div className='buttons'>
+                <a href={topBeer.brewery.mapURL} target='new'><button className='mapDirection btn btn-primary'>Directions</button></a>  
+
                 <button className={props.userFavs.indexOf(topBeer._id) === -1 ? "saveBeer btn btn-primary": "saveBeer btn btn-primary disabled"} id={topBeer._id} onClick={(event) => props.handleSavedButton(topBeer)}>
                     {props.userFavs.indexOf(topBeer._id) === -1 ? "Favorite": "Favorited"}
                 </button>
+                </div>
                 </Row>
               </li>
+              </div>
+              </div>
             );
           })}
         </div>
       </div>
+    </div>
     )
 }
 
