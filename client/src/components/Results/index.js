6dc2f740bf76b212133ad8fd24b58604f589416b
Sarah B <smblair87@gmail.com>
2019-08-03 18:06:03 -0700

    Set up grid for beer cards on search results and added style

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -15,10 +15,10 @@ const SearchResults = props => {
   ) : ( 
       <div className='cards'>
         <div className='card-body'>
-          <div className='result'>
+          <div className='result grid-container'>
             {props.beers.map(beer => {
               return (
-                <div className='border'>
+                <div className='border fluid'>
                 <li className='search-list list-group-item' key={beer._id}>
                   <Row className='SearchResult row' id={beer.name + 'Card'}>
                     <Col size='2' className='beerImage'>
@@ -29,31 +29,24 @@ const SearchResults = props => {
                     </Col>
                     <Col size='1' className='emptyCol' />
                     <Col size='9' className='beerInfo'>
+                      <Row className='beerInfo'>
 
-                      <Row>
                         <h5 className='beerName'>{beer.name}</h5>
-                      </Row>
-
-                      <Row>
+                 
                         <h6 className='abv'>ABV: {beer.abv}</h6>
-                      </Row>
-                      <Row>
-                        <p className='description'>{beer.description}</p>
-                      </Row>
-                      <Row>
+                     
+                        <p className='description'>{beer.description}</p>     
+               
                         <p className='breweryName'>Brewery: {beer.brewery.name}</p>
-                      </Row>
-                      <Row>
+               
                         <p className='breweryweb'>Website: {beer.brewery.website}</p>
-                      </Row>
-                      <Row>
-                                          
-                      <a href="https://google.com"><p className='brewerylocation'>Brewery Address: {beer.breweryLocation}</p></a>
+                                         
+                        <a href="https://google.com"><p className='brewerylocation'>Brewery Address: {beer.breweryLocation}</p></a>
+
                       </Row>
                     </Col>
                   </Row>
 
-                  <br></br>
                   <Row className='buttonDiv'>
                     <button className='saveBeer btn btn-primary' id={beer.id} onClick={(event) => props.handleSavedButton(beer)}>
                       Favorite
